92ea0f12e855114b54a6523232fc6f62209beb3b
Sadab Halim <sadabhalim.sh@gmail.com>
2026-01-26 17:31:43 +0530

    remove

--- Structural/src/decorator/ea/Coffee.java
+++
@@ -1,8 +0,0 @@
-package decorator.ea;
-
-// 1. Component Interface
-// This defines the common interface for both concrete components and decorators.
-public interface Coffee {
-    String getDescription(); // Describes the coffee
-    double getCost();        // Returns the cost of the coffee
-}
